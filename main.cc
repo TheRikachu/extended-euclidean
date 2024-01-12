f692e652fdddedc5b813c46628be64b4187cac2d
TheRikachu <richard.porthan@hotmail.com>
2024-01-12 15:52:38 +0200

    Replace PrintPoly with PolyString

--- main.cc
+++ main.cc
@@ -12,7 +12,7 @@ std::tuple<int, int, int ,int> GCD(int f, int g)
     int s = 0;
     int t = 1;
 
-    std::cout << "i: 0" << ", r: " << rLast << ", s: " << sLast << ", t: " << tLast << std::endl;
+    std::cout << "i: 0, r: " << rLast << ", s: " << sLast << ", t: " << tLast << std::endl;
 
     int i = 1;
     while (r != 0)
@@ -38,32 +38,64 @@ std::tuple<int, int, int ,int> GCD(int f, int g)
     return std::make_tuple(l, r, s, t);
 }
 
-void PrintPoly(std::vector<int> poly)
+std::string PolyString(std::vector<int> poly)
 {
+    std::string str = "";
     if (poly[poly.size() - 1]) {
-        std::cout << "x^" << poly.size() - 1;
+       str.append("x^" + std::to_string(poly.size() - 1));
     }
     for(int i = poly.size() - 2; i >= 0; --i)
     {
         if (i == 0 && poly[0])
         {
-            std::cout << " + 1";
+            str.append(" + 1");
         }
         else if (i == 1 && poly[1])
         {
-            std::cout << " + x";
+            str.append(" + x");
         }
         else if (poly[i]) {
-            std::cout << " + x^" << i;
+            str.append(" + x^" + std::to_string(i));
         }
     }
-    std::cout << std::endl;
+    return str;
 }
 
-std::tuple<int, int, int, int> GCD(std::vector<int> f, std::vector<int> g, int mod)
+/*std::tuple<int, int, int, int> GCD(std::vector<int> f, std::vector<int> g, int mod)
 {
+    auto rLast = f;
+    auto sLast = {1};
+    auto tLast = {0};
+
+    auto r = g;
+    auto s = {0};
+    auto t = {1};
+
+    std::cout << "i: 0, r: " << PolyString(rLast) << ", s: " << PolyString(sLast) << ", t: " << PolyString(tLast) << std::endl;
+
+    int i = 1;
+    while (r != 0)
+    {
+        int rHelp = r;
+        int sHelp = s;
+        int tHelp = t;
+
+        int q = rLast / r;
+        std::cout << "i: " << i << ", r: " << r << ", s: " << s << ", t: " << t << ", q: " << q << std::endl;
+        r = rLast - q * r;
+        s = sLast - q * s;
+        t = tLast - q * t;
+
+        rLast = rHelp;
+        sLast = sHelp;
+        tLast = tHelp;
+        i++;
+    }
+    std::cout << "i: " << i << ", r: " << r << ", s: " << s << ", t: " << t << std::endl;
+    int l = i - 1;
+
     return std::make_tuple(0, 0, 0, 0);
-}
+}*/
 
 auto main(int argc, char *argv[]) -> int {
     std::cout << "::Integer GCD::" << std::endl;
@@ -89,10 +121,8 @@ auto main(int argc, char *argv[]) -> int {
     std::vector<int> fPoly = {1, 1, 0, 1, 1};
     std::vector<int> gPoly = {1, 0, 0, 0, 1};
 
-    std::cout << "fPoly = ";
-    PrintPoly(fPoly);
-    std::cout << "gPoly = ";
-    PrintPoly(gPoly);
+    std::cout << "fPoly = " << PolyString(fPoly) << std::endl;
+    std::cout << "gPoly = " << PolyString(gPoly) << std::endl;
 
     return 0;
 }
